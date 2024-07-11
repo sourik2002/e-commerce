a5f8d474f6d2f8385ef51398a5622b2d632d2c1e
sourik2002 <sourik2002@gmail.com>
2024-07-11 10:58:51 +0530

    all

---
+++ scriptsheet/catagories.js
@@ -0,0 +1,9 @@
+let cartQuantity = 0;
+if(cartQuantity==0){
+    document.querySelector('#item-count').style.visibility='hidden';
+}
+else
+{
+    document.querySelector('#item-count').style.display= cartQuantity;
+}
+document.querySelector('#item-count').innerText = `${cartQuantity}`;
---
+++ scriptsheet/index.js
@@ -0,0 +1,4 @@
+let cartQuantity = 0;
+let favQuantity = 0;
+document.querySelector('#item-count').innerText = `${cartQuantity}`;
+document.querySelector('#items-count').innerText = `${favQuantity}`;
